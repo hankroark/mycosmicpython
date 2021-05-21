a217a3cb88150c068114ba2fd3a60346dfe227ae
Hank Roark <hank.roark@gmail.com>
2021-05-21 11:21:50 -0700

    Added ability to deallocate and allocate lines Required Batch to keep set of allocated lines    Properties changes appropriately
    Also, fixed the expected/actual order in unit tests assertEquals to correct order (definitely help with debugging)

--- model.py
+++ model.py
@@ -15,11 +15,27 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
 
     def allocate(self, line: OrderLine):
         if self.can_allocate(line):
-            self.available_quantity -= line.qty
+            self._allocations.add(line)
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.available_quantity >= line.qty and self.sku == line.sku
+
+    def deallocate(self, line: OrderLine):
+        if self.can_deallocate(line):
+            self._allocations.remove(line)
+
+    def can_deallocate(self, line: OrderLine) -> bool:
+        return line in self._allocations
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
--- test_batches.py
+++ test_batches.py
@@ -16,12 +16,12 @@ class MyTestCase(unittest.TestCase):
         batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
         line = OrderLine("order-ref", "SMALL-TABLE", 2)
         batch.allocate(line)
-        self.assertEqual(batch.available_quantity, 18)
+        self.assertEqual(18, batch.available_quantity)
 
     def test_does_not_allocate_if_line_bigger_than_batch(self):
         small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
         small_batch.allocate(large_line)
-        self.assertEqual(small_batch.available_quantity, 2)
+        self.assertEqual(2, small_batch.available_quantity)
 
     def test_can_allocate_if_available_greater_than_required(self):
         large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
@@ -39,6 +39,18 @@ class MyTestCase(unittest.TestCase):
         batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
         different_sku_line = OrderLine("order-123", "EXPENSIVE_TOASTER", 10)
         self.assertFalse(batch.can_allocate(different_sku_line))
+
+    def test_can_only_deallocate_allocated_lines(self):
+        batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+        batch.deallocate(unallocated_line)
+        self.assertEqual(20, batch.available_quantity)
+
+    def test_can_deallocate_allocated_lines(self):
+        batch, line = make_batch_and_line("DECORATIVE-TRINKET", 2, 2)
+        batch.allocate(line)
+        self.assertEqual(0, batch.available_quantity)
+        batch.deallocate(line)
+        self.assertEqual(2, batch.available_quantity)
 
 
 if __name__ == '__main__':
